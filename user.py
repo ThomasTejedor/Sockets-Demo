3a989e68491fd14d28a6b46f1ae9632844559a0b
Thomas Tejedor <tctejedor@yahoo.com>
2025-05-09 21:15:40 -0700

    User portion of image sending

--- user.py
+++ user.py
@@ -8,13 +8,18 @@ name = input("what's your name? ")
 host = '192.168.254.52'
 port = 5000
 
-img_encoded = None
-img_bytes = None
+class ImageHandler:
+    receivedRequest = False
+    receivedLength = False
 
 client = socket.socket()
 client.connect((host,port))
 
+
+
 def sendImage(args):
+    ImageHandler.receivedRequest = False
+    ImageHandler.receivedLength = False
     if len(args) > 1 or len(args) == 0 : 
         print("Incorrect usage try \"/sendImage (image path)\" use")
         return
@@ -35,26 +40,67 @@ def sendImage(args):
         #Get array of bytes from the image
         nparr = np.array(img_encoded)
         img_bytes = nparr.tobytes()
-        message = "img"
+        message = 'img'
         
-        #Send img process request
+            
         client.send(message.encode())
+        while not ImageHandler.receivedRequest:
+            ()
         #send length of img
         client.send(str(len(img_bytes)).encode())
+        
+        while not ImageHandler.receivedLength:
+            ()
+            
         #send image
         client.sendall(img_bytes)
-        
 
+#Displays image from buffer
+def displayImage(image):
+    nparr = np.frombuffer(image,np.byte)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imshow("cpp image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+#Receives image from server then handles the data
+def receiveImage(length):
+    #Let server know you are ready to receive image data
+    msg = 'imgRec'
+    client.send(msg.encode())
+    
+    data = b''
+    #receive image from server
+    while True:
+        bytes = client.recv(1024)
+        data += bytes
+        
+        if(len(data)) == length:
+            break
+    
+    thread = threading.Thread(target = displayImage, args=(data,))
+    thread.start()
+    
+    
+    
 # receives message from server
 # also sends user if asked
+
 def receive():
     while True:
         try:
             msg = client.recv(1024).decode()
-            
             if msg == 'name?':
                 client.send(name.encode())
-
+            #acknowledge server received image request
+            elif msg == 'ackImgReq':
+                ImageHandler.receivedRequest = True
+            #acknowledge server received length
+            elif msg == 'ackImgLength':
+                ImageHandler.receivedLength = True
+            #If an img is being sent to user
+            elif len(msg) > 7 and msg[:7] == 'imgSent':
+                receiveImage(int(msg[7:]))
             else:
                 print(msg)
             
